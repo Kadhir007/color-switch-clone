65cbbd74ec4fe79b37112ff122f7ced8fd3b88bb
Kadhir007 <kadhir8526@gmail.com>
2021-05-03 13:31:26 +0530

    A color switch game made with unity

---
+++ Assets/scripts/playbutton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class playbutton : MonoBehaviour
+{
+    // changes scence when play is pressed
+
+    public void OnMouseDown()
+    {
+        SceneManager.LoadScene("playscence");
+    }
+}
---
+++ Assets/scripts/scoreHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scoreHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject currentscore;
+    
+
+    private TextMesh currentscoretext;
+   
+
+   
+    void Start()
+    {
+        //targeting mesh components
+        currentscoretext = currentscore.GetComponent<TextMesh>();
+      
+        //changing current score
+        currentscoretext.text = PlayerPrefs.GetString("currentscore");
+        //checking for high score
+        
+        
+    } 
+
+
+    
+}
